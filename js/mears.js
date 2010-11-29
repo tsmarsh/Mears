433d748333453acf4b474c63c1d7f51cdeb25d5e
Tom Marsh <ts.marsh@gmail.com>
2010-11-29 07:40:31 -0500

    [Tom] A little bit of clean up

--- js/mears.js
+++ js/mears.js
@@ -1,16 +1,31 @@
 var sandbridge;
 var defaultPath = '/Users/tom/Desktop/bugs/';
 
-var saved = function(){
+var saved = function(face){
 	alert("saved!");
-}
+};
+
+var show_list = function(list){
+    var bugs = eval(list);
+    var objects = [];
+    $(bugs).each(function(i, bug){
+        objects.push({'bug': bug});        
+    });
+    $.tmpl( "list_template", objects ).appendTo( "#list ol" );
+    
+};
+
 $(function(){
 	sandbridge = document.sandbridge;
+    $("#list_template").template("list_template");
+
+
 	$('#track_button').click(function(){
 		var filename = defaultPath + $('#write').val() + ".json";
 		var contents = $('#contents').val();
-		alert(filename + " : " + contents);
 		sandbridge.save(
 			filename, contents, 'saved');
 	});
+
+    sandbridge.list(defaultPath, 'show_list');
 });
